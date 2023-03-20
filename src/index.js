02094913019efbc4f33f71471036fb787a62b6cf
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-03-20 15:47:41 +0900

    Refactor: basic router setting

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Root from '@pages/index';
-
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
